ec457f3167ebe8bf82452d1d176cd10688420b34
Daniel Martinez <danielj.martinez@memorandum.net>
2022-11-20 21:08:44 +0100

    Task-1: done

--- src/main/java/acme/entities/Blahblah.java
+++ src/main/java/acme/entities/Blahblah.java
@@ -1,8 +1,11 @@
 package acme.entities;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -19,42 +22,45 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class TheoryTutorial extends AbstractEntity {
 
+public class Blahblah extends AbstractEntity {
+	
 	// Serialisation identifier -----------------------------------------------
-
-	protected static final long	serialVersionUID	= 1L;
-
+	
+	protected static final long serialVersionUID = 1L;
+	
 	// Attributes -------------------------------------------------------------
 
 	@NotBlank
-	@Length(max = 76)
-	protected String			title;
-
-	@NotBlank
 	@Column(unique = true)
-	@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(:[A-Z]{1,10})?$")
-	protected String			ticker;
+	@Pattern(regexp = "^\\w{3}-\\d{6}$")
+	protected String ticker;
+	
+	@NotBlank
+	@Length(max = 75)
+	protected String caption;
 
 	@NotBlank
 	@Length(max = 255)
-	protected String			abstractText;
+	protected String summary;
 
 	@NotNull
 	@Valid
-	protected Money				cost;
+	protected Money cost; 
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date creationMoment;
 
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date initDate;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date finishDate;
+	
 	@URL
-	protected String			link;
+	protected String hlink;
 	
-	protected boolean publish;
-
-	// Derived attributes -----------------------------------------------------
-
-	// Relationships ----------------------------------------------------------
-	
-	@Valid
-	@OneToOne(optional=true)
-	protected Blahblah blahblah;
-
 }
